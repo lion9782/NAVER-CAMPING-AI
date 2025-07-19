004c486de84859ae2b32d2cd8f92d35bb1911b56
'cksrn' <'cksrn37@naver.com'>
2025-07-19 21:10:07 +0900

    1. swagger 연동

---
+++ src/main/java/com/ncloud/swagger/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.ncloud.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+public class SwaggerConfig {
+	
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ncloud.ai"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SAMPLE-API")
+                .description("샘플 API 입니다.")
+                .termsOfServiceUrl("coding.toast.co.kr/api/")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public UiConfiguration uiconfig() {
+        return UiConfigurationBuilder
+                .builder().operationsSorter(OperationsSorter.ALPHA)
+                .build();
+    }
+}
